b373544b062040d8741cc3f72b7bb9ac307b2a0b
willi-dev <willi.ilmukomputer@gmail.com>
2018-07-11 23:41:19 +0700

    add bootstrap, reactstrap, node-sass

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App container">
+        <header className="App-header col-md-5">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
