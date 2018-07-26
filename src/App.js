50796764c877f3a4c77d15aaf57d5737940adaf6
willi-dev <willi.ilmukomputer@gmail.com>
2018-07-26 22:09:48 +0700

    add header navbar bootstrap; add state menu collapse

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
 
-
-
 class App extends Component {
   render() {
     return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 
+const INITIAL_STATE = {
+	'isMenuOpen': false,
+}
+
 class Header extends Component {
+
+	constructor( props ) {
+		super(props);
+		this.state = {...INITIAL_STATE};
+		this.onClickMenu = this.onClickMenu.bind(this);
+	}
+
+	onClickMenu = () => {
+		this.setState({
+			isMenuOpen: !this.state.isMenuOpen,
+		})
+	}
+
 	render() {
+		const { isMenuOpen } = this.state;
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			  <a className="navbar-brand" href="">Navbar</a>
-			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			  <a className="navbar-brand" href="">React App</a>
+			  <button onClick={this.onClickMenu} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			    <span className="navbar-toggler-icon"></span>
 			  </button>
 
-			  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+			  <div className={ isMenuOpen ? 'collapse show navbar-collapse': 'collapse navbar-collapse'} id="navbarSupportedContent">
 			    <ul className="navbar-nav mr-auto">
 			      <li className="nav-item active">
-			        <a className="nav-link" href="">Home 
-			        <span className="sr-only">(current)</span></a>
+			        <a className="nav-link" href="">Link 1</a>
 			      </li>
 			      <li className="nav-item">
-			        <a className="nav-link" href="">Link 1</a>
+			        <a className="nav-link" href="">Link 2</a>
 			      </li>
 			      <li>
-			        <a className="nav-link" href="">Link 1</a>
+			        <a className="nav-link" href="">Link 3</a>
 			      </li>
 			     
 			    </ul>
